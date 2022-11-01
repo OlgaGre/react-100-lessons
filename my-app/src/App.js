d1933edabe56c45c1fac58a59880b8d3538fcb7c
OlgaGre <grechanyuko@gmail.com>
2022-11-01 15:52:22 +0300

    feat: html markup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,38 @@
 //import logo from './logo.svg';
 import "./App.css";
-import Header from "./Header";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Header />
-      simple html
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Social_media_icon_noun_33167_-_Modified.svg" />
+        header
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <main classNmae="main">
+        <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/free-social-icons.png" />
+        <div>avatar</div>
+        <div>descr</div>
+        <div>new post</div>
+        <div> post</div>
+        main
+      </main>
     </div>
   );
 };
