a019270882bc5b5c184d49320847a5319d07a47a
OlgaGre <grechanyuko@gmail.com>
2022-11-01 16:07:04 +0300

    refactor: separation into components

---
+++ my-app/src/components/Heder.jsx
@@ -0,0 +1,10 @@
+ 
+ const Header = () =>{
+  return (
+    <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Social_media_icon_noun_33167_-_Modified.svg" />
+        header
+      </header>
+  )
+ }
+ export default Header
---
+++ my-app/src/components/Main.jsx
@@ -0,0 +1,14 @@
+const Main = () =>{
+  return(
+    <main classNmae="main">
+    <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/free-social-icons.png" />
+    <div>avatar</div>
+    <div>descr</div>
+    <div>new post</div>
+    <div> post</div>
+    main
+  </main>
+  )
+}
+
+export default Main
