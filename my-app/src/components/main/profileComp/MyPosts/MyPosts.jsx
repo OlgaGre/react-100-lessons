988bf9fb245ad2cfe65825d46162541b84f9b7e8
OlgaGre <grechanyuko@gmail.com>
2022-11-03 15:08:12 +0300

    feat: add textarea

--- my-app/src/components/main/profileComp/MyPosts/MyPosts.jsx
+++ my-app/src/components/main/profileComp/MyPosts/MyPosts.jsx
@@ -3,6 +3,10 @@ import Post from "./Post/Post";
 const MyPosts = () => {
   return (
     <div className={classes.myPosts}>
+      <div className="newPost" >
+        <textarea name="new post" id="" cols="30" rows="3"></textarea>
+        <button>add</button>
+      </div>
       <Post message='1 post' />
       <Post message='2 post' />
       <Post message='3 post'/>
